6f75e4f7c88c737f32b6c1399fd84bc01e4f23c0
hoahp <hoahp@alphasoftware.vn>
2024-06-22 09:46:28 +0700

    update popular routes

--- src/components/PopularRoad/index.jsx
+++ src/components/PopularRoad/index.jsx
@@ -5,8 +5,12 @@ import 'swiper/css/navigation';
 // import required modules
 import { Navigation, Autoplay } from 'swiper/modules';
 import { useSelector } from "react-redux";
+import { API_STORE } from '../../configs/apis';
+import { useNavigate } from 'react-router-dom';
+import { ROUTER } from '../../configs/router';
 
 const PopularRoad = () => {
+    const navigate = useNavigate();
     const swiperParams = {
         navigation: true,
         slidesPerView: 4,
@@ -24,23 +28,38 @@ const PopularRoad = () => {
     };
     const { routes } = useSelector(state => state.routes);
     return (
-        <section className="my-4">
-            <p className="font-semibold text-base xl:text-2xl text-[#3d3d3b] py-2.5">Tuyến đường phổ biến</p>
-            <Swiper {...swiperParams} navigation={true} modules={[Navigation, Autoplay]} className="mySwiper">
-                {routes.map(d => (
-                    <SwiperSlide key={d.id}>
-                        <div className="w-11/12 shadow-primary rounded cursor-pointer">
-                            <div className="w-full h-32 rounded-lt rounded-tr relative">
-                                <img className="w-full h-full rounded-tl rounded-tr absolute" src={d.img} alt="" />
-                            </div>
-                            <div className={"p-2.5 rounded-bl rounded-br"} style={{ backgroundColor: d.color }}>
-                                <p className="text-white font-semibold">{d.name}</p>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </section>
+        <>
+            {(routes.filter(d => d.popular === 1).length > 0) && (
+                <section className="my-4">
+                    <p className="font-semibold text-base xl:text-2xl text-[#3d3d3b] py-2.5">Tuyến đường phổ biến</p>
+                    <Swiper {...swiperParams} navigation={true} modules={[Navigation, Autoplay]} className="mySwiper">
+                        {routes.filter(d => d.popular === 1).map(d => (
+                            <SwiperSlide key={d.id}>
+                                <div className="w-11/12 shadow-primary rounded cursor-pointer" onClick={() => {
+                                    navigate(ROUTER.SEARCH, {
+                                        state: {
+                                            departure_province_id: '',
+                                            return_province_id: '',
+                                            start_date: '',
+                                            end_date: '',
+                                            sort: 'asc_time',
+                                            route_id: d.id
+                                        }
+                                    });
+                                }}>
+                                    <div className="w-full h-32 rounded-lt rounded-tr relative">
+                                        <img className="w-full h-full rounded-tl rounded-tr absolute" src={`${API_STORE}${d.image}`} alt="" />
+                                    </div>
+                                    <div className={"p-2.5 rounded-bl rounded-br bg-blacktransparent"}>
+                                        <p className="text-white font-semibold">{d.name}</p>
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </section>
+            )}
+        </>
     );
 }
 
